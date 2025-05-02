19e7b23a22752c058b9cdb94eb5fccd801c28cd8
vacosta <vacosta@ues.com.uy>
2025-05-02 20:43:47 -0300

    prueba mongo

--- src/models/mongo.client.js
+++ src/models/mongo.client.js
@@ -1,24 +1,14 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectMongoDB = async () => {
-  const uri    = process.env.MONGODB_URI; 
-  const dbName = process.env.MONGODB_DATABASE_NAME;
+  const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+  const MONGODB_DATABASE_NAME = process.env.MONGODB_DATABASE_NAME;
 
-  if (!uri) {
-    throw new Error('⚠️ La variable MONGODB_URI no está definida en el entorno');
-  }
-
-  try {
-    await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 3000,
-      dbName,
-    });
-    console.log('✅ Conexión a MongoDB establecida correctamente');
-  } catch (error) {
-    console.error('✘ Error al conectar a MongoDB:', error);
-    throw error;
-  }
+  await mongoose.connect(`${MONGODB_CONNECTION_STRING}`, {
+    serverSelectionTimeoutMS: 3000,
+    dbName: MONGODB_DATABASE_NAME,
+  });
+  console.log("Conexión a MongoDB establecida correctamente");
 };
 
 module.exports = connectMongoDB;
--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -1,21 +1,19 @@
-const { Redis } = require('@upstash/redis')
+const { Redis } = require("@upstash/redis");
 
-const REDIS_URL = process.env.REDIS_URL
-const REDIS_TOKEN = process.env.REDIS_TOKEN
+const REDIS_URL = process.env.REDIS_URL;
+const REDIS_TOKEN = process.env.REDIS_TOKEN;
+
 let redisClient = null;
 
-const connectToRedis = () =>{
-    if(!redisClient){
-        const connectionOpts ={
-            url: REDIS_URL,
-            token: REDIS_TOKEN
-        };
-        redisClient = new Redis(connectionOpts);
-    }
-    return redisClient;
-}
+const connectToRedis = () => {
+  if (!redisClient) {
+    const connectionOpts = {
+      url: REDIS_URL,
+      token: REDIS_TOKEN,
+    };
+    redisClient = new Redis(connectionOpts);
+  }
+  return redisClient;
+};
 
 module.exports = connectToRedis;
-
-// await redis.set('foo', 'bar');
-// const data = await redis.get('foo');

5b1b7545568a441f0de3a8071af5d69aa32da5dd
vacosta <vacosta@ues.com.uy>
2025-05-03 18:29:44 -0300

    swagger

--- src/routes/public.router.js
+++ src/routes/public.router.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
-const fs = require("fs");
-const path = require("path");
 const publicRouter = express.Router();
-const swaggerFile = fs.readFileSync(path.resolve(__dirname, "../public/swagger.json"));
-const swaggerDocument = JSON.parse(swaggerFile);
+const path = require('path');
+const swaggerDocument = require(path.join(__dirname, '../public/swagger.json'));
 
 const {
   healthController,

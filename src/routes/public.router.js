2e26611a2e49a62f63be92b1eb6dd647ad983955
vacosta <vacosta@ues.com.uy>
2025-05-03 23:11:55 -0300

    test 5

--- src/routes/public.router.js
+++ src/routes/public.router.js
@@ -11,19 +11,24 @@ const {
 publicRouter.get("/health", healthController);
 publicRouter.get("/ping", pingController);
 
+const swaggerOptions = {
+  explorer: true,
+  customCss: '.swagger-ui .topbar { display: none }',
+  swaggerOptions: {
+    docExpansion: 'list',
+    persistAuthorization: true,
+    filter: true
+  },
+  customJs: [
+    'https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js',
+    'https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js'
+  ],
+  customCssUrl: 'https://unpkg.com/swagger-ui-dist/swagger-ui.css'
+};
+
 publicRouter.use("/swagger", swaggerUi.serve);
 publicRouter.get("/swagger", (req, res) => {
-  const options = {
-    explorer: true,
-    customCss: '.swagger-ui .topbar { display: none }',
-    swaggerOptions: {
-      docExpansion: 'list',
-      persistAuthorization: true,
-      filter: true
-    }
-  };
-  
-  res.send(swaggerUi.generateHTML(swaggerDocument, options));
+  res.send(swaggerUi.generateHTML(swaggerDocument, swaggerOptions));
 });
 
 publicRouter.get("/swagger.json", (req, res) => {

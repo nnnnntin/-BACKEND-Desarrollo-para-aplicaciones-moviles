b3cf9bb5974e15dc2096fe35d5a182155d25122f
vacosta <vacosta@ues.com.uy>
2025-05-03 23:05:42 -0300

    test 2

--- src/app.js
+++ src/app.js
@@ -55,11 +55,7 @@ const app = express();
 app.use(express.json());
 //app.use(loggerMiddleWare);
 app.use(morgan("dev"));
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
+app.use(cors());
 app.use(sanitizeMiddleware);
 
 app.use("/", publicRoutes);

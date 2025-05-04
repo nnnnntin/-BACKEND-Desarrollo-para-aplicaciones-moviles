226e20c9679a01bf5a42c2767b11cdc1926d8e1e
vacosta <vacosta@ues.com.uy>
2025-05-03 23:07:43 -0300

    test 3

--- src/app.js
+++ src/app.js
@@ -55,7 +55,11 @@ const app = express();
 app.use(express.json());
 //app.use(loggerMiddleWare);
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', PUT, 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 app.use(sanitizeMiddleware);
 
 app.use("/", publicRoutes);
--- src/routes/public.router.js
+++ src/routes/public.router.js
@@ -9,9 +9,25 @@ const {
 } = require("../controllers/public.controller");
 
 publicRouter.get("/health", healthController);
-
 publicRouter.get("/ping", pingController);
 
-publicRouter.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+publicRouter.use("/swagger", swaggerUi.serve);
+publicRouter.get("/swagger", (req, res) => {
+  const options = {
+    explorer: true,
+    customCss: '.swagger-ui .topbar { display: none }',
+    swaggerOptions: {
+      docExpansion: 'list',
+      persistAuthorization: true,
+      filter: true
+    }
+  };
+  
+  res.send(swaggerUi.generateHTML(swaggerDocument, options));
+});
+
+publicRouter.get("/swagger.json", (req, res) => {
+  res.json(swaggerDocument);
+});
 
 module.exports = publicRouter;

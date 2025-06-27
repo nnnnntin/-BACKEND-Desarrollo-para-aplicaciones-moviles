186c63d6964808b20fe5f82bdfe7ac237bad97ca
vacosta <vacosta@ues.com.uy>
2025-06-26 22:04:55 -0300

    addons

--- src/routes/empresaInmobiliarias.router.js
+++ src/routes/empresaInmobiliarias.router.js
@@ -27,38 +27,100 @@ const {
   verificarEmpresaSchema
 } = require("./validations/empresaInmobiliaria.validation");
 
-router.get("/", getEmpresasInmobiliariasController);
-router.get("/verificadas", getEmpresasVerificadasController);
-router.get("/tipo/:tipo", getEmpresasByTipoController);
-router.get("/ciudad/:ciudad", getEmpresasByCiudadController);
-router.get("/ranking/espacios", getEmpresasConMasEspaciosController);
-router.get("/:id", getEmpresaInmobiliariaByIdController);
-router.get("/usuario/:usuarioId", getEmpresaByUsuarioIdController);
-
 router.post(
-  "/",
+  "/registro",
   payloadMiddleware(createEmpresaInmobiliariaSchema),
   createEmpresaInmobiliariaController
 );
 
+router.get(
+  "/empresas-inmobiliarias",
+  getEmpresasInmobiliariasController
+);
+
+router.get(
+  "/empresas-inmobiliarias/verificadas",
+  getEmpresasVerificadasController
+);
+
+router.get(
+  "/empresas-inmobiliarias/tipo/:tipo",
+  getEmpresasByTipoController
+);
+
+router.get(
+  "/empresas-inmobiliarias/ciudad/:ciudad",
+  getEmpresasByCiudadController
+);
+
+router.get(
+  "/empresas-inmobiliarias/ranking/espacios",
+  getEmpresasConMasEspaciosController
+);
+
+router.get(
+  "/empresas-inmobiliarias/:id",
+  getEmpresaInmobiliariaByIdController
+);
+
 router.use(authMiddleware);
 
+router.post(
+  "/empresas-inmobiliarias",
+  payloadMiddleware(createEmpresaInmobiliariaSchema),
+  createEmpresaInmobiliariaController
+);
+
 router.put(
-  "/:id",
+  "/empresas-inmobiliarias/:id",
   payloadMiddleware(updateEmpresaInmobiliariaSchema),
   updateEmpresaInmobiliariaController
 );
-router.delete("/:id", deleteEmpresaInmobiliariaController);
-router.put("/:id/activar", activarEmpresaInmobiliariaController);
+
+router.delete(
+  "/empresas-inmobiliarias/:id",
+  deleteEmpresaInmobiliariaController
+);
+
+router.put(
+  "/empresas-inmobiliarias/:id/activar",
+  activarEmpresaInmobiliariaController
+);
+
 router.post(
-  "/verificar",
+  "/empresas-inmobiliarias/verificar",
   payloadMiddleware(verificarEmpresaSchema),
   verificarEmpresaController
 );
-router.put("/:id/calificacion", actualizarCalificacionController);
-router.post("/:id/espacio", agregarEspacioController);
-router.delete("/:id/espacio/:espacioId", eliminarEspacioController);
-router.put("/:id/metodo-pago", actualizarMetodoPagoController);
-router.put("/:id/contacto", actualizarContactoController);
+
+router.put(
+  "/empresas-inmobiliarias/:id/calificacion",
+  actualizarCalificacionController
+);
+
+router.post(
+  "/empresas-inmobiliarias/:id/espacio",
+  agregarEspacioController
+);
+
+router.delete(
+  "/empresas-inmobiliarias/:id/espacio/:espacioId",
+  eliminarEspacioController
+);
+
+router.put(
+  "/empresas-inmobiliarias/:id/metodo-pago",
+  actualizarMetodoPagoController
+);
+
+router.put(
+  "/empresas-inmobiliarias/:id/contacto",
+  actualizarContactoController
+);
+
+router.get(
+  "/empresas-inmobiliarias/usuario/:usuarioId",
+  getEmpresaByUsuarioIdController
+);
 
 module.exports = router;

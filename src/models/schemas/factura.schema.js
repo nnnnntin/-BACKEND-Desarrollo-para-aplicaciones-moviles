6d625095d63e48493a2f6bbbfa5c11f1eace4774
vacosta <vacosta@ues.com.uy>
2025-06-28 19:43:38 -0300

    addons

--- src/models/schemas/factura.schema.js
+++ src/models/schemas/factura.schema.js
@@ -4,7 +4,10 @@ const facturaSchema = new mongoose.Schema(
   {
     numeroFactura: { type: String, required: true, unique: true },
     usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-    emisorId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    emisorId: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true
+    },
     tipoEmisor: { type: String, enum: ['plataforma', 'inmobiliaria', 'proveedor'], required: true },
     fechaEmision: { type: Date, required: true },
     fechaVencimiento: { type: Date, required: true },
